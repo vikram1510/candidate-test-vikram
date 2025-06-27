9664a9dbb986177c0cd03774117066a884872397
Vikram Bageja <vikram.bageja@gmail.com>
2025-06-27 17:02:13 +0100

    Fix type

--- src/shared/useWorkStatusSync.ts
+++ src/shared/useWorkStatusSync.ts
@@ -1,17 +1,18 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addWorkStatusListener, EventSource } from "./events";
+import { Action } from "@reduxjs/toolkit";
 import { WorkStatus } from "./types";
 
 // Custom hook to listen for work status changes from other stores
 export const useWorkStatusSync = (
-  updateAction: (status: WorkStatus) => any,
+  updateAction: (payload: WorkStatus) => Action & { meta?: object },
   currentSource: EventSource
 ) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const cleanup = addWorkStatusListener((event) => {
+    const cleanup = addWorkStatusListener(event => {
       // Only update if event came from another source
       if (event.detail.source !== currentSource) {
         // Mark this action as coming from sync to prevent middleware from dispatching another event
